e3254ca1b75954c493a1faa831caa2b51e95f889
Z24r <36693705+gushengsheng@users.noreply.github.com>
2024-11-25 12:40:48 +0800

    Add files via upload

--- z24r‘s statistic/statistic1.R
+++ z24r‘s statistic/statistic1.R
@@ -15,7 +15,7 @@ ylen <- length(y1)
 average_x <- sum(x1)/xlen
 average_y <- sum(y1)/ylen
 x2 <- x1-average_x
-y2 <- x1-average_y
+y2 <- y1-average_y
 x3 <- x2*x2
 y3 <- y2*y2
 multiplication <- x2*y2
